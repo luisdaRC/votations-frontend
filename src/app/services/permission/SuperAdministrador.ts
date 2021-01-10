b07ef8fcdf7a8cd7fc23da673da7f9ae15fb32f8
luisdaRC <luixdani123@gmail.com>
2021-01-10 00:28:16 -0500

    Módulo inicio de administrador empezando a funcionar

---
+++ src/app/services/permission/SuperAdministrador.ts
@@ -0,0 +1,22 @@
+export const SUPER_ADMINISTRADOR = [ // En este administrador seguir las rutas del admin de estos manes
+  {
+    title: 'REGISTRAR',
+    subtitles: [
+      {
+        name:  'Anuncio',
+        icon: 'fas fa-ad',
+        route: '/admin/registry/ad'
+      },
+      {
+        name:  'Revisor fiscal',
+        icon: 'fas fa-revisor',
+        route: '/admin/registry/revisor'
+      },
+      {
+        name:  'Secretario de asamblea',
+        icon: 'fas fa-secretary',
+        route: '/admin/registry/secretary'
+      }
+    ]
+  }
+];
