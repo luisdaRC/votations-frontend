c551a0725481680b20707d1ceff3178e64b69813
luisdaRC <luixdani123@gmail.com>
2021-01-23 23:48:08 -0500

    Duda desde perspectiva del negocio.

--- src/app/services/permission/RevisorFiscal.ts
+++ src/app/services/permission/RevisorFiscal.ts
@@ -1,4 +1,7 @@
-export const REVISOR_FISCAL = [ // En este revisor seguir las rutas del propitario de estos manes
+// Revisar desde la perspectiva del negocio, cómo se debe realizar este caso de uso.
+// ¿Sólo se le mostrará la última votación y los propietarios que votaron o los resultados
+// de todas las votaciones que se han realizado en la PH?.
+export const REVISOR_FISCAL = [
   {
     title: 'LISTAR',
     subtitles: [
