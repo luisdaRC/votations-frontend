bc78d519aa9c95ccd277c7c619f23dd9d8f68088
luisdaRC <luixdani123@gmail.com>
2021-01-24 18:20:05 -0500

    Duda desde perspectiva del negocio.

--- src/app/services/permission/RevisorFiscal.ts
+++ src/app/services/permission/RevisorFiscal.ts
@@ -1,7 +1,7 @@
 // Revisar desde la perspectiva del negocio, cómo se debe realizar este caso de uso.
-// ¿Sólo se le mostrará la última votación y los propietarios que votaron o los resultados
-// de todas las votaciones que se han realizado en la PH?.
 // UPDATE: Realiza revisión del proceso cuando no hay más mociones en la asamblea. Cuando acaba(?)
+// Consultar fechaFin para saber si una asamblea está en curso y si ya se le pueden mostrar los resultados al revisor
+// (it means, cuando la asamblea haya finalizado)
 export const REVISOR_FISCAL = [
   {
     title: 'LISTAR',
