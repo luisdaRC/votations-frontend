479d7f772c46e53bf66ff3ca1904c88736e82a87
luisdaRC <54014615+luisdaRC@users.noreply.github.com>
2021-01-24 00:57:24 -0500

    Update RevisorFiscal.ts
    Aclaración revisor respecto al desempeño de su actividad en el negocio

--- src/app/services/permission/RevisorFiscal.ts
+++ src/app/services/permission/RevisorFiscal.ts
@@ -1,6 +1,7 @@
 // Revisar desde la perspectiva del negocio, cómo se debe realizar este caso de uso.
 // ¿Sólo se le mostrará la última votación y los propietarios que votaron o los resultados
 // de todas las votaciones que se han realizado en la PH?.
+// UPDATE: Realiza revisión del proceso cuando no hay más mociones en la asamblea. Cuando acaba(?)
 export const REVISOR_FISCAL = [
   {
     title: 'LISTAR',
