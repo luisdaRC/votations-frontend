e46a12cf6394157981e229baf819f5159f4c89af
luisdaRC <luixdani123@gmail.com>
2021-02-23 00:31:11 -0500

    Setting up server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 //Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/votations-frontend'));
+app.use(express.static('./dist/votations-frontend'));
 
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/votations-frontend/index.html'));
-});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/votations-frontend/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
